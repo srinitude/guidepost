66f8a757ee378e8264acd9b2aef0cdd2cdaf82b3
Kiren Srinivasan <srinitude@gmail.com>
2019-05-07 14:07:55 -0700

    Adds temporary source root

--- lib/generators/guidepost/models_generator.rb
+++ lib/generators/guidepost/models_generator.rb
@@ -6,6 +6,10 @@ module Guidepost
         class ModelsGenerator < Rails::Generators::Base
             desc "This generator creates the migrations, models, and tests associated with your current Zendesk guides"
 
+            def self.default_generator_root
+                File.dirname(__FILE__)
+            end
+
             def migrations
                 timestamp = Time.now.strftime('%Y%m%d%H%M%S')
                 template "migrations.rb", "db/migrate/#{timestamp}_create_knowledge_base_guide_models"                
